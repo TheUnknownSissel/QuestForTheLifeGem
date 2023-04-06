d4b60c2c4ccf6d8a38b0039f1be3cc64b326109a
TheUnknowSissel <Theunknownsissel@gmail.com>
2023-04-05 20:18:56 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#import random
+#import webbrowser
+import pygame
+#import os
+
+#Initialize the pygame
+pygame.init()
+#
+class Unit:
+    def __init__(self, name, health):
+        self.name = name
+
+#Create the window
+background_colour = (0, 133, 66)
+screen_width = 1000
+screen_height = 1000
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption('Quest For the Life Gem')
+
+pygame.display.flip()
+
+running = True
+
+#create the screen
+White = (0, 0, 0)
+CYAN = (0, 100, 100)
+MAGENTA = (255, 0, 255)
+#pygame.draw.rect(screen, White, screen.Rect(200, 200, 100, 50))
+#pygame.draw.rect(screen, CYAN, screen.Rect(300, 150, 100, 100))
+#pygame.draw.rect(screen, MAGENTA, (400, 100, 100, 150))
+
+
