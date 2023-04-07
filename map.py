54533cd6b6c6b6cf1edb5d1b5c634e86758d7ffd
TheUnknowSissel <Theunknownsissel@gmail.com>
2023-04-07 14:24:29 -0500

    added a map and mob file. needs more work and fiddling. I also added a texture that we can try and work with here soon

---
+++ map.py
@@ -0,0 +1,12 @@
+import pygame, sys
+
+grass = 0
+tree = 1
+
+
+textures = {
+             grass : pygame.image.load('Textures/downdload.jpeg'),
+             tree  : pygame.image.load('Textures/tree.png')
+
+           }
+
