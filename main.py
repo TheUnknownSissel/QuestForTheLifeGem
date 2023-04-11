dc7a7f3db1197d356dbf19bb5afd39ea52896e55
TheUnknowSissel <Theunknownsissel@gmail.com>
2023-04-10 19:16:35 -0500

    tried to pulll changes for unit py did not work will try again. did testing in main py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pygame.event as Events
 #import os
 import sys
 import unit
+import mob
 
 
 
@@ -27,14 +28,21 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 MAGENTA = (255, 0, 255)
+#Initialize Units
+#mage = unit("MAGE", 70, 3, 1, 3, -5, 10)
+#warrior = unit("Warrior", 100, 8, 5, 2, 10, 0)
+#tank = unit("Tank", 200, 1, 12, 7, 0, 0)
 
-mage = unit("MAGE", 70, 3, 1, 3, -5, 10)
-warrior = unit("Warrior", 100, 8, 5, 2, 10, 0)
-tank = unit("Tank", 200, 1, 12, 7, 0, 0)
+
+#Create list of these units
+#units = [warrior, mage, tank]
+
+#Initialize mobs
+#list here
+
+#Turn order sort both lists on speed
 
 
-
-units = [warrior, mage, tank]
 
 #screen start up
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -45,8 +53,5 @@ pygame.display.flip()
 
 running = True
 
-
-
-#pygame.draw.rect(screen, White, screen.Rect(200, 200, 100, 50))
-#pygame.draw.rect(screen, CYAN, screen.Rect(300, 150, 100, 100))
+pygame.draw.rect(screen, WHITE, )
 #pygame.draw.rect(screen, MAGENTA, (400, 100, 100, 150))
