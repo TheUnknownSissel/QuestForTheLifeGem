203e8f40c4a6b6516dbe80a478da3d544fa34c86
kquinn9 <kquinn9@luc.edu>
2023-04-10 19:08:37 -0500

    Added getters and setters

--- unit.py
+++ unit.py
@@ -4,14 +4,53 @@ import pygame
 import string
 import sys
 
+
 class Unit:
-    def __init__(self, name, health, speed, defen, res, phy, mag):
+    def __init__(self, name, health, speed, defen, res, atk, type):
         self.name = string(name)
         self.health = int(health)
         self.speed = int(speed)
         self.defen = int(defen)
         self.res = int(res)
-        self.phy = int(phy)
-        self.mag = int(mag)
-    def getname():
-        return name
+        self.atk  = int(atk)
+        self.type = string(type)
+
+    def set_name(self, n):
+        self.name = n
+
+    def set_health(self, h):
+        self.health = h
+
+    def set_speed(self, s):
+        self.speed = s
+
+    def set_defen(self, d):
+        self.defen = d
+
+    def set_res(self, r):
+        self.res = r
+
+    def set_atk(self, a):
+        self.atk = a
+    def set_type(self):
+        self.type = t
+
+    def get_name(self):
+        return self.name
+
+    def get_health(self):
+        return self.health
+
+    def get_speed(self):
+        return self.speed
+
+    def get_defen(self):
+        return self.defen
+
+    def get_res(self):
+        return self.res
+
+    def get_atk(self):
+        return self.atk
+    def get_type(self):
+        return self.type
